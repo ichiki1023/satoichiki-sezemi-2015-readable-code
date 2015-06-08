f40c708e9633cc1ee461c1a5e8a029ffbfdab438
Shun Nishitsuji <nishitsuji_shun@liginc.co.jp>
2015-06-08 17:48:56 +0900

    処理をクラス化

--- task/recipe.php
+++ task/recipe.php
@@ -1,30 +1,44 @@
 <?php
-	$recipe_array = array();
-	$file_path = $argv[1];
-	$count_id = 1; //idを割り振るためのカウント
+	class Recipe {
+		public $params;
 
-	$file = fopen($file_path, "r");
+		public function printRecipe($argv) {
+			$this->params = $argv;
+			$recipe_array = $this->addRecipeID();
+			$recipe_num = $this->params[2]; // 入力されたレシピID
+			// IDが指定されているかチェックする。
+			if(isset($recipe_num)) {
+				// IDが指定されている場合
+				echo $recipe_array[$recipe_num];
+			} else {
+				// IDが指定されていない場合はレシピを全表示する
+				for ($i = 1; $i <= count($recipe_array); $i++) {
+					echo $recipe_array[$i];
+				}
+				echo "\n";
+			}
+		}
 
-	/*1行ずつファイルを読み込み出力する*/
-	if($file){
-		while($line = fgets($file)){
-			// レシピにIDを付与する
-			$recipe_array[$count_id] = $count_id.": ".$line;
-			$count_id++;
+		private function addRecipeID() {
+			$recipe_array = array();
+			$file_path = $this->params[1];
+
+			$file = fopen($file_path, "r");
+
+			/*1行ずつファイルを読み込み出力する*/
+			$count_id = 1; //idを割り振るためのカウント
+
+			if($file){
+				while($line = fgets($file)){
+					// レシピにIDを付与する
+					$recipe_array[$count_id] = $count_id.": ".$line;
+					$count_id++;
+				}
+			}
+			fclose($file);
+			return $recipe_array;
 		}
 	}
-	fclose($file);
 
-	$recipe_num = $argv[2]; // 入力されたレシピID
-
-	// IDが指定されているかチェックする。
-	if(isset($recipe_num)) {
-		// IDが指定されている場合
-		echo $recipe_array[$recipe_num];
-	} else {
-		// IDが指定されていない場合はレシピを全表示する
-		for ($i = 1; $i <= count($recipe_array); $i++) {
-			echo $recipe_array[$i];
-		}
-		echo "\n";
-	}
+	$recipe = new Recipe;
+	$recipe->printRecipe($argv);
